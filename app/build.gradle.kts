2e5f043d750b55cfbe03e7e567c027a09d103310
shivamtechstack <devshivamyadav1604@gmail.com>
2024-11-18 14:48:46 +0530

    Basic UI created

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "shivam.sycodes.securepeek"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "shivam.sycodes.securepeek"
         minSdk = 31
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
