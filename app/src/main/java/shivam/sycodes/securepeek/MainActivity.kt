9911006dd0168ceafac50e2605281b22016b03e7
shivamtechstack <devshivamyadav1604@gmail.com>
2024-11-19 13:16:01 +0530

    SafeBrowsingService (API), okhttp

--- app/src/main/java/shivam/sycodes/securepeek/MainActivity.kt
+++ app/src/main/java/shivam/sycodes/securepeek/MainActivity.kt
@@ -13,6 +13,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import shivam.sycodes.securepeek.databinding.ActivityMainBinding
 import shivam.sycodes.securepeek.metadata.URLMetadataFetcher
+import shivam.sycodes.securepeek.network.SafeBrowsingService
 import shivam.sycodes.securepeek.utils.WebViewScreenshotService
 
 class MainActivity : AppCompatActivity() {
@@ -34,29 +35,49 @@ class MainActivity : AppCompatActivity() {
             val url = binding.receivedURL.text.toString().trim()
             if (url.isNotEmpty()) {
                 updateProgressAndErrors("Analysing URL...")
-                binding.ContentResult.visibility = View.GONE
-                binding.urlScreenshot.visibility = View.GONE
-                binding.urlPreview.visibility = View.GONE
                 analyseURL(url)
                 updateProgressAndErrors("Loading Preview...")
                 loadPreview(url)
             } else {
                 updateProgressAndErrors("Please enter a valid URL")
+                clearEverything()
             }
+        }
+        binding.clearbutton.setOnClickListener {
+            binding.receivedURL.text?.clear()
+            binding.progressAndErrorsText.text= ""
+            clearEverything()
         }
     }
 
+    @SuppressLint("SetTextI18n")
     private fun analyseURL(url: String) {
         CoroutineScope(Dispatchers.IO).launch {
             try {
+                val checksafety = SafeBrowsingService.checkUrlSafety(url)
+                val threats = checksafety.threats ?: emptyList()
                 val metadata = URLMetadataFetcher.fetchURLMetadata(url)
+
                 withContext(Dispatchers.Main) {
-                    binding.ContentResult.text = "Title: ${metadata.title}\n\nDescription: ${metadata.description}"
-                    binding.ContentResult.visibility = View.VISIBLE
-                    metadata.image?.let {
-                        binding.urlPreview.setImageBitmap(it)
-                        binding.urlPreview.visibility = View.VISIBLE
+                    binding.threatAnalyseResult.text = "URL is ${if (checksafety.isSafe) "safe" else "unsafe"} \n\n" +
+                            "Number of Threats : ${threats.size} threats \n\nThreat types : ${threats.map { it.type }}"
+                    binding.threatAnalyseContainer.visibility = View.VISIBLE
+                }
+
+                withContext(Dispatchers.Main) {
+                    if (metadata != null) {
+                        binding.threatAnalyseContainer.visibility = View.VISIBLE
+                        binding.ContentResult.visibility = View.VISIBLE
+                        binding.ContentResult.text = "Title: ${metadata.title}\n\nDescription: ${metadata.description}"
+                        metadata.image?.let {
+                            binding.urlPreview.setImageBitmap(it)
+                            binding.urlPreview.visibility = View.VISIBLE
+                        }
+                    } else {
+                        binding.ContentResult.text = "Metadata could not be fetched for the URL."
                     }
+
+                    binding.ContentResult.visibility = View.VISIBLE
                     updateProgressAndErrors("")
                 }
             } catch (e: Exception) {
@@ -66,14 +87,13 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
     private fun loadPreview(url: String) {
         WebViewScreenshotService.loadUrlInBackground(this, url) { screenshot ->
             runOnUiThread {
                 if (screenshot != null) {
                     binding.urlScreenshot.setImageBitmap(screenshot)
-                    binding.urlScreenshot.visibility = View.VISIBLE
-                    updateProgressAndErrors("") // Clear progress text
+                    binding.urlScreenshotContainer.visibility = View.VISIBLE
+                    updateProgressAndErrors("")
                 } else {
                     updateProgressAndErrors("Website preview failed.")
                 }
@@ -83,5 +103,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun updateProgressAndErrors(message: String) {
         binding.progressAndErrorsText.text = message
+    }
+    private fun clearEverything() {
+        binding.threatAnalyseContainer.visibility = View.GONE
+        binding.threatAnalyseResult.text = ""
+        binding.AIResult.text = ""
+        binding.ContentResult.text = ""
+        binding.AIResultContainer.visibility = View.GONE
+        binding.ContentResult.visibility = View.GONE
+        binding.urlPreview.visibility = View.GONE
+        binding.urlScreenshotContainer.visibility = View.GONE
     }
 }
